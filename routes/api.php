83cda030167e238c98801616328e9c87ee6f01c5
Sengkea <119031980+Sengkea2022@users.noreply.github.com>
2026-04-20 00:32:43 +0700

    update: add api test route and test

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'Backend API is connected.',
+        'status' => 'ok',
+    ]);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
